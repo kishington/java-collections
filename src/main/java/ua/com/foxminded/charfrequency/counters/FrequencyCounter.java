a0e61f729d9cfc53dc6fe4576d627d9f23d834e9
Mihail Iarov <yarovmk@gmail.com>
2019-11-14 00:29:59 +0000

    Fixed getFrequencies method

--- src/main/java/ua/com/foxminded/charfrequency/counters/FrequencyCounter.java
+++ src/main/java/ua/com/foxminded/charfrequency/counters/FrequencyCounter.java
@@ -1,7 +1,6 @@
 package ua.com.foxminded.charfrequency.counters;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -10,16 +9,13 @@ public class FrequencyCounter {
     Map<String, String> cache = new LinkedHashMap<>();
 
     public String getFrequencies(String input) {
-        cache.computeIfAbsent(input, k -> {
-            List<Character> chars = input.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-
+        String freqsToDisplay = cache.computeIfAbsent(input, k -> {
             Map<Character, Integer> frequencies = new LinkedHashMap<>();
-            chars.forEach(c -> frequencies.merge(c, 1, Integer::sum));
-
-            return frequencies.keySet().stream().map(key -> "\"" + key + "\" - " + frequencies.get(key) + "\n")
+            input.chars().mapToObj(c -> (char) c).collect(Collectors.toList()).forEach(c -> frequencies.merge(c, 1, Integer::sum));
+            String freqsToCache = frequencies.keySet().stream().map(key -> "\"" + key + "\" - " + frequencies.get(key) + "\n")
                     .collect(Collectors.joining());
+            return freqsToCache;
         });
-
-        return cache.get(input);
+        return freqsToDisplay;
     }
 }
