2bb1a09c82ddffc1cdf5d141d2fe5f5ff8acdb36
Mihail <yarovmk@gmail.com>
2019-11-14 15:46:20 +0000

    Formatted streams a bit

--- src/main/java/ua/com/foxminded/charfrequency/counters/FrequencyCounter.java
+++ src/main/java/ua/com/foxminded/charfrequency/counters/FrequencyCounter.java
@@ -4,6 +4,9 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.joining;
+
 public class FrequencyCounter {
 
     Map<String, String> cache = new LinkedHashMap<>();
@@ -11,9 +14,15 @@ public class FrequencyCounter {
     public String getFrequencies(String input) {
         String result = cache.computeIfAbsent(input, k -> {
             Map<Character, Integer> frequencies = new LinkedHashMap<>();
-            input.chars().mapToObj(c -> (char) c).collect(Collectors.toList()).forEach(c -> frequencies.merge(c, 1, Integer::sum));
-            String freqsToCache = frequencies.keySet().stream().map(key -> "\"" + key + "\" - " + frequencies.get(key) + "\n")
-                    .collect(Collectors.joining());
+            input.chars()
+            	 .mapToObj(c -> (char) c)
+            	 .collect(toList())
+            	 .forEach(c -> frequencies.merge(c, 1, Integer::sum));
+            
+            String freqsToCache = frequencies.keySet()
+            								 .stream()
+            								 .map(key -> "\"" + key + "\" - " + frequencies.get(key) + "\n")
+            								 .collect(joining());
             return freqsToCache;
         });
         return result;
