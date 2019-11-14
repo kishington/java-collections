ab1e9bf5055619b15431f4f715d3248a8c61d08b
Mihail <yarovmk@gmail.com>
2019-11-14 15:13:30 +0000

    Renamed freqsToDisplay to result

--- src/main/java/ua/com/foxminded/charfrequency/counters/FrequencyCounter.java
+++ src/main/java/ua/com/foxminded/charfrequency/counters/FrequencyCounter.java
@@ -9,13 +9,13 @@ public class FrequencyCounter {
     Map<String, String> cache = new LinkedHashMap<>();
 
     public String getFrequencies(String input) {
-        String freqsToDisplay = cache.computeIfAbsent(input, k -> {
+        String result = cache.computeIfAbsent(input, k -> {
             Map<Character, Integer> frequencies = new LinkedHashMap<>();
             input.chars().mapToObj(c -> (char) c).collect(Collectors.toList()).forEach(c -> frequencies.merge(c, 1, Integer::sum));
             String freqsToCache = frequencies.keySet().stream().map(key -> "\"" + key + "\" - " + frequencies.get(key) + "\n")
                     .collect(Collectors.joining());
             return freqsToCache;
         });
-        return freqsToDisplay;
+        return result;
     }
 }
